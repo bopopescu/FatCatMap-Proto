1b98811ebaa383c60991f43e4de293be17f5ba67
Bogdan Popescu <68062990+bopopescu@users.noreply.github.com>
2020-07-25 23:14:37 +0300

    Migrate from master/slave terminology

--- app/lib/ProvidenceClarity/data/core/model.py
+++ app/lib/ProvidenceClarity/data/core/model.py
@@ -3,7 +3,7 @@ from google.appengine.ext import db
 
 class Model(db.Model):
 
-    """ Root, master, non-polymorphic data model. Everything lives under this class. """
+    """ Root, main, non-polymorphic data model. Everything lives under this class. """
     
     def _getModelPath(self,seperator=None):
 
